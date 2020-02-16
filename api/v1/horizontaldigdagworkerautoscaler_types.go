88a306842449ae2346ff7f296939d4e5dbd94ac0
TrsNium <trsnium@gmail.com>
2020-02-16 18:23:56 +0900

    fix comment

--- api/v1/horizontaldigdagworkerautoscaler_types.go
+++ api/v1/horizontaldigdagworkerautoscaler_types.go
@@ -20,9 +20,7 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
 // NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
-
 // HorizontalDigdagWorkerAutoscalerSpec defines the desired state of HorizontalDigdagWorkerAutoscaler
 type HorizontalDigdagWorkerAutoscalerSpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
