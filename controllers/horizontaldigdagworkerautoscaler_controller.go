95322bbdaa690714c1cf88b421740aee31a7bc97
TrsNium <trsnium@gmail.com>
2020-02-18 13:30:35 +0900

    add config

--- controllers/horizontaldigdagworkerautoscaler_controller.go
+++ controllers/horizontaldigdagworkerautoscaler_controller.go
@@ -38,7 +38,7 @@ type HorizontalDigdagWorkerAutoscalerReconciler struct {
 // +kubebuilder:rbac:groups=horizontalpodautoscalers.autoscaling.digdag-worker-crd,resources=horizontaldigdagworkerautoscalers,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=horizontalpodautoscalers.autoscaling.digdag-worker-crd,resources=horizontaldigdagworkerautoscalers/status,verbs=get;update;patch
 // +kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;list;watch;create;update;delete
-
+// +kubebuilder:rbac:groups="",resources=configmap,verbs=get;list;watch;create;update;delete
 func (r *HorizontalDigdagWorkerAutoscalerReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
 	ctx := context.Background()
 	log := r.Log.WithValues("HorizontalDigdagWorkerAutoscaler", req.NamespacedName)
